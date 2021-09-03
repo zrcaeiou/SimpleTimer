2f3f83f63ed89e7631469918687fc09278542ddf
Zhang Ruichao <richozhang@gmail.com>
2021-09-03 12:57:16 +0800

    change

--- Timer1.py
+++ Timer1.py
@@ -10,7 +10,7 @@ def gettime():
 
       timeInterval=time.time()-targetTime#计算时间差
       timestr = time.strftime("%D-%H:%M:%S") # 获取当前的时间并转化为字符串
-      lb.configure(text='当前时间:'+timestr+'\n\n玻璃固化热试已连续运行\n'+time.strftime("%d天%H小时%M分%S秒",time.gmtime(timeInterval)))   # 重新设置标签文本
+      lb.configure(text='当前时间:'+timestr+'\n\n已连续运行\n'+time.strftime("%d天%H小时%M分%S秒",time.gmtime(timeInterval)))   # 重新设置标签文本
       root.after(1000,gettime) # 每隔1s调用函数 gettime 自身获取时间
 
 def main():
