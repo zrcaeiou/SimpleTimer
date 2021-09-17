18c3a9e7a2cdaf183f6c6ee7d35760271d8b0ff3
Zhang Ruichao <richozhang@gmail.com>
2021-09-17 15:35:15 +0800

    修改错误的计时方式。

--- Timer1.py
+++ Timer1.py
@@ -10,9 +10,9 @@ targetTime=time.mktime(time.strptime("19 52 15 27 8 2021","%H %M %S %d %m %Y"))
 lb = tkinter.Label(root,text='',width=w,height=h,fg='#f0f0f0',font=("黑体",80),bg='#f01020')
 
 def gettime():
-      timeInterval=time.time()-targetTime-86400#计算时间差
+      timeInterval=int(time.time()-targetTime)#计算时间差
       timestr = time.strftime("%Y-%m-%d %H:%M:%S") # 获取当前的时间并转化为字符串
-      lb.configure(text='\n当前时间:'+timestr+'\n\n热调试已连续运行\n\n'+time.strftime("%d天%H小时%M分%S秒",time.gmtime(timeInterval)))   # 重新设置标签文本
+      lb.configure(text='\n当前时间:'+timestr+'\n\n热调试已连续运行\n\n'+str(int(timeInterval/86400))+'天'+str(int(timeInterval%86400/3600))+'小时'+str(int(timeInterval%3600/60))+'分钟'+str(int(timeInterval%60))+'秒')   # 重新设置标签文本
       root.after(1000,gettime) # 每隔1s调用函数 gettime 自身获取时间
 
 def main():
